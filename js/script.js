9ce95a80591e0dfc7bfbfcb00aa2e8610928fa58
SiaKovalina <siakovalina@gmail.com>
2017-03-19 13:17:00 +0200

    smooth scroll and working links added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    /*Sticky Nav*/
     $('.js--features').waypoint(function(direction) {
         if (direction == 'down') {
             $('nav').addClass('sticky');
@@ -11,10 +12,29 @@ $(document).ready(function() {
 });
     
     
+    /*Scroll buttons*/
+$('.js--scroll-to-plans').click(function (){
+    $('html, body').animate({scrollTop: $('.js--plans').offset().top}, 1000);
+});
     
-       /* var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
-}, {
-  offset: '25%'
-}) */
+$('.js--scroll-to-start').click(function (){
+    $('html, body').animate({scrollTop: $('.js--features').offset().top}, 1000);
+});
+    
+    /*Nav scroll*/
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 });
